989ea2d0235fb4d6ba74bb021e65dcc615e0b0a5
avemadison <avemadison@rambler.ru>
2016-11-22 23:32:13 +0200

    game

---
+++ Snake/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespase Snake {
+    class Program 
+    {
+        static void Main (string[] args)
+        {
+            int x1 = 1;
+            int y2 = 3;
+            char sym1 = '*';
+            
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(sym1);
+            
+            Console.RedLine();
+        }
+    }
+}
