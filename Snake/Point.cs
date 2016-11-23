2c9dd1af042d9952aa4286174245a71ec80409b8
Sofi <avemadison@rambler.ru>
2016-11-23 21:46:04 +0200

    Create class Point

---
+++ Snake/Point.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespase Snake
+ {
+     class Point {
+     public int x;
+     public int y;
+     public char sym;
+     
+     public void Draw 
+     {
+     Console.SetCursorPosition(x1, y1);
+             Console.Write(sym1);
+     }
+    }
+ }
+
+
+
