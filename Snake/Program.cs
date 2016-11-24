8db62dfe3a71a49f665f4833e15343ecda4a45b0
Sofi <avemadison@rambler.ru>
2016-11-24 19:46:37 +0200

    rew

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,18 +11,44 @@ namespase Snake {
           static void Main (string[] args)
           {
 
-            Point p1 = new Point;
-             p1.x = 1;
-             p1.y = 3;
-             p1.sym = '*';
-             p1.Draw();
+             List<int> numList = new List <int>();
+             numList.Add(0);
+             numList.Add(1);
+             numList.Add(2);
+            
+            int x = numList [0];
+            int y = numList [1];
+            int z = numList [2];
+            
+            foreach (int i in numList){
+                Console.WriteLine (i);
+            }
+              numList = RemoveAt[0];
               
- 
-             Point p2 = new Point;
-             p2.x = 4;
-             p2.y = 5;
-             p2.sym = '#';
-             p2.Draw();
+              List<char> charList = new List<char>();
+              charList.Add('*');
+              charList.Add('#');
+              charList.Add('@');
+              charList.Add('$');
+              
+              foreach (char r in charList) {
+                  Console.WriteLine (r);
+              }
+              
+             Point p1 = new Point(1, 3, '*');
+             Point p2 = new Point(4, 5, '#');
+             Point p3 = new Point(6, 1, '@');
+             Point p4 = new Point(7, 2, '$');
+             
+             List<Point> pList = new List<Point>();
+             pList.Add(p1);
+             pList.Add(p2);
+             pList.Add(p3);
+             pList.Add(p4);
+              
+              foreach (Point p in pList){
+                  p.Draw();
+              }
               
               Console.RedLine();
           }
