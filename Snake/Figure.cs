407f3b7d8f921c26bd56c028831d72f32d7c9abb
Sofi <avemadison@rambler.ru>
2016-11-25 23:28:33 +0200

    create

---
+++ Snake/Figure.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespase Snake {
+    class Figure 
+    {
+    protected List <Point> pList;
+ 
+     public void Draw(){
+        foreach(Point p in pList)
+        {
+            p.Draw();
+        }
+    }
+    }
