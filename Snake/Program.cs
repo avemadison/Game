e780bc374bba8f18e584e37bb99974a17d082c3c
Sofi <avemadison@rambler.ru>
2016-11-26 21:58:35 +0200

    commit

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -11,51 +11,65 @@ namespase Snake {
           static void Main (string[] args)
           {
 
-             List<int> numList = new List <int>();
-             numList.Add(0);
-             numList.Add(1);
-             numList.Add(2);
+             Console.SetBufferSize (80,25);
+              //отрисовка рамки
+              HorizontalLine upLine = new HorizonalLine (0, 78, 0, '+');
+              HorizontalLine dawnLine = new HorizontalLine (0, 78, 24, '+');
+              VerticalLine leftLine = new VerticalLine (0, 24, 0, '+');
+              VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+              upLine.Drow();
+              dawnLine.Drow();
+              leftLine.Drow();
+              rightLine.Drow();
+              
+              //отрисовка точек
+              Point p = new Point(4,5,'*');
+              p.Draw();
+//              List<int> numList = new List <int>();
+//              numList.Add(0);
+//              numList.Add(1);
+//              numList.Add(2);
             
-            int x = numList [0];
-            int y = numList [1];
-            int z = numList [2];
+//             int x = numList [0];
+//             int y = numList [1];
+//             int z = numList [2];
             
-            foreach (int i in numList){
-                Console.WriteLine (i);
-            }
-              numList = RemoveAt[0];
+//             foreach (int i in numList){
+//                 Console.WriteLine (i);
+//             }
+//               numList = RemoveAt[0];
               
-              List<char> charList = new List<char>();
-              charList.Add('*');
-              charList.Add('#');
-              charList.Add('@');
-              charList.Add('$');
+//               List<char> charList = new List<char>();
+//               charList.Add('*');
+//               charList.Add('#');
+//               charList.Add('@');
+//               charList.Add('$');
               
-              foreach (char r in charList) {
-                  Console.WriteLine (r);
-              }
+//               foreach (char r in charList) {
+//                   Console.WriteLine (r);
+//               }
               
-             Point p1 = new Point(1, 3, '*');
-             Point p2 = new Point(4, 5, '#');
-             Point p3 = new Point(6, 1, '@');
-             Point p4 = new Point(7, 2, '$');
+//              Point p1 = new Point(1, 3, '*');
+//              Point p2 = new Point(4, 5, '#');
+//              Point p3 = new Point(6, 1, '@');
+//              Point p4 = new Point(7, 2, '$');
              
-             List<Point> pList = new List<Point>();
-             pList.Add(p1);
-             pList.Add(p2);
-             pList.Add(p3);
-             pList.Add(p4);
+//              List<Point> pList = new List<Point>();
+//              pList.Add(p1);
+//              pList.Add(p2);
+//              pList.Add(p3);
+//              pList.Add(p4);
               
-              foreach (Point p in pList){
-                  p.Draw();
-              }
+//               foreach (Point p in pList){
+//                   p.Draw();
+//               }
               
-              Console.RedLine();
-          }
-      }
- }
+//               Console.RedLine();
+//           }
+//       }
+//  }
             
-            Console.RedLine();
-        }
-    }
+//             Console.RedLine();
+//         }
+//     }
 }
