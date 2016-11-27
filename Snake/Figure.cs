92275635d8c80c8aca6b0917bf1a0ea435b64a0f
Sofi <avemadison@rambler.ru>
2016-11-27 14:18:49 +0200

    rework

--- Snake/Figure.cs
+++ Snake/Figure.cs
@@ -4,15 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespase Snake {
-    class Figure 
-    {
-    protected List <Point> pList;
- 
-     public void Draw(){
-        foreach(Point p in pList)
-        {
-            p.Draw();
-        }
-    }
-    }
+namespace Snake
+{
+	class Figure
+	{
+		protected List<Point> pList;
+
+		public void Draw()
+		{
+			foreach ( Point p in pList )
+			{
+				p.Draw();
+			}
+		}
+
+		internal bool IsHit( Figure figure )
+		{
+			foreach(var p in pList)
+			{
+				if ( figure.IsHit( p ) )
+					return true;
+			}
+			return false;
+		}
+
+		private bool IsHit( Point point )
+		{
+			foreach(var p in pList)
+			{
+				if ( p.IsHit( point ) )
+					return true;
+			}
+			return false;
+		}
+	}
+}
